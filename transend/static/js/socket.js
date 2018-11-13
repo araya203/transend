32bbfda6dca77cf7d05595f9c9319950d57d5ec2
EC2 Default User <ec2-user@ip-172-31-24-40.eu-west-2.compute.internal>
2018-11-13 16:46:23 +0000

    TR-8: solving done on download issue | TR-18: split javascript | TR-9: Add link to generate new qr code + better download method and project structure

--- transend/static/js/socket.js
+++ transend/static/js/socket.js
@@ -1,30 +1,58 @@
 var ipport = "35.178.176.41";
-console.log(ipport);
+var socket = io.connect();
 $(document).ready(function() {
-	var socket = io.connect();
-        console.log("CONNECTED");
         socket.emit('browserconnect');
+
+        $("#qrcodecontainer").append("<a href='#'>Generate another QR code</a>");
+
         socket.on('file', function(filejson) {
-        	var filename = filejson['filename'];
-                console.log(filename);
-                $("#qrcodecontainer").children('img').attr('src', 'static/images/done.jpg');
-                window.location.replace("http://transendtest.com/getfile/"+filename);
+                var filename = filejson['filename'];
+                var session_id = filejson['session_id'];
+                var request = new XMLHttpRequest();
+                request.open('GET', '/getfile/'+session_id+'/'+filename, true)
+                request.responseType = 'blob';
+
+                request.onload = function() {
+                        if(request.status === 200) {
+                                var disposition = request.getResponseHeader('content-disposition');
+                                var matches = /"([^"]*)"/.exec(disposition);
+                                var filename2 = (matches != null && matches[1] ? matches[1] : filename);
+                                var blob = new Blob([request.response], { type: 'appliaction/octet-stream' });
+                                var link = document.createElement('a');
+                                link.href = window.URL.createObjectURL(blob);
+                                link.download = filename2;
+
+                                document.body.appendChild(link);
+                                link.click();
+                                 document.body.removeChild(link);
+                        }
+                };
+                request.send();
         });
-	socket.on('link', function(urljson) {
-		var url = urljson['url'];
-		document.location = url;
-	});
+
+        $("a").on('click', function(event) {
+                 event.preventDefault();
+                 socket.emit('browserconnect');
+        });
+
+        socket.on('link', function(urljson) {
+                var url = urljson['url'];
+                document.location = url;
+        });
+
         socket.on('qrpath', function(qrjson) {
+                var id = socket.io.engine.id;
                 var qrpath = qrjson['QR'];
-                console.log(qrpath);
-                $("#qrcodecontainer").append("<img src='"+qrpath+"'/>");
+                $("#qrcodecontainer").children('img').attr('src', qrpath);
         });
+
         socket.on('loading', function(loadjson) {
                 var isloading = loadjson['isloading'];
-                if(isloading) {
-        	        console.log(isloading);
+                if(isloading == true) {
                         $("#qrcodecontainer").children('img').attr('src', 'static/images/loadinggif.gif');
                 }
-
+                else {
+                        $("#qrcodecontainer").children('img').attr('src', 'static/images/done.jpg');
+                }
         });
 });
